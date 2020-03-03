57d8daf75f0694157ad34da95a9256f9610fa185
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-03-03 19:15:30 +0900

    deviseでuserTableを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles, only: [:index,:new,:create,:show,:destroy,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
