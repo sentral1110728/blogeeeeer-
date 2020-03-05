7e94581fba1834dd67861152b3f79ebd4650a693
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-03-05 17:54:41 +0900

    profileTable作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_145504) do
+ActiveRecord::Schema.define(version: 2020_03_05_083226) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_03_04_145504) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "appeal"
+    t.string "user_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_03_04_145504) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "profiles", "users"
 end
