99099ffb246fc766f9bd7413769e95f13d2078a3
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-03-12 18:23:07 +0900

    未ログイン者はいいね機能を利用できない

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
+  # articleが削除されたときarticleに紐付くlikeも削除
   has_many :likes, dependent: :destroy
   belongs_to :user
   has_many :comments
-  
+
   def like_user(id)
     likes.find_by(user_id: id)
   end
