12f0624c795761ac9dfb45a6ffd8e0bd166d72e9
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-20 15:25:52 +0900

    デザイン調整・カテゴリの表示を可変にする

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,7 @@
 class TopsController < ApplicationController
   def index
     @categories = Category.all
+    @categories_slice = @categories.each_slice(3).to_a
+    puts @categories_slice.length
   end
 end
