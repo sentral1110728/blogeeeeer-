cd95b13b819cdfb08ad01ada5571c684c4068f7e
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-23 15:21:00 +0900

    カテゴリー一覧画面にいいねの総数とコメントの総数を表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,16 @@
 class CategoriesController < ApplicationController
   def index
+    # TODO SQLでまとめたい
     @categories = Category.all
+    @category_likes_count =
+      Category
+        .joins(articles: :likes)
+        .group("categories.id")
+        .count
+    @category_comments_count =
+      Category
+        .joins(articles: :comments)
+        .group("categories.id")
+        .count
   end
 end
