0f2f95d6bde7d7ba7c7761d3bf90af6f12ea1268
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-24 15:44:55 +0900

    カテゴリーをDBへ登録

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,5 +15,18 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @new_category = Category.new
+  end
+
+  def create
+    create_category = Category.new(category_params)
+    if create_category.save
+      redirect_to categories_path, notice: '新規カテゴリーを作成しました'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:category_name, :image)
   end
 end
