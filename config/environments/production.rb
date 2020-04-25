c0ad04112d5ad7a8dabc2873b4014d621281c1ce
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-25 17:55:00 +0900

    自動デプロイ設定の記述ファイルミス

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  server '13.230.224.20', user: 'ec2-user', roles: %w{app db web}
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
