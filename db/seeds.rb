9e6f8e2b321059eac98bd483b1b506ea1c36fa4c
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-26 10:46:19 +0900

    管理者用のuserデータをseedで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Authority.create!(id: 1, name: "一般")
 Authority.create!(id: 2, name: "管理者")
+User.create!(nickname:  ENV['nickname'],
+  email: ENV['email'],
+  password:  ENV['password'],
+  password_confirmation: ENV['password_confirmation'],
+  authority_id: ENV['authority_id'])
