c89cb72a3bacf8bf873b0d2ad12a920b7d19bc28
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-26 19:25:33 +0900

    管理者用のprofileのデータをseedで登録する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Authority.create!(id: 1, name: "一般")
+# Authority.create!(id: 1, name: "一般")
 Authority.create!(id: 2, name: "管理者")
-User.create!(nickname:  ENV['nickname'],
+user = User.create!(nickname:  ENV['nickname'],
   email: ENV['email'],
   password:  ENV['password'],
   password_confirmation: ENV['password_confirmation'],
   authority_id: ENV['authority_id'])
+Profile.create!(user_id: user.id,
+  appeal: ENV['appeal'])
