610e92c12528186f0f04f5b03555739087b0890a
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-04-26 19:59:16 +0900

    seeds.rb内の一部コメントアウト解除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Authority.create!(id: 1, name: "一般")
+Authority.create!(id: 1, name: "一般")
 Authority.create!(id: 2, name: "管理者")
 user = User.create!(nickname:  ENV['nickname'],
   email: ENV['email'],
