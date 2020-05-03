9a3048659f51e0200532a9e1baef65c0fda2b59f
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-03 11:40:39 +0900

    validation内容の設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   has_many :articles
   mount_uploader :image, ImageUploader
+
+  validates :category_name, 
+    presence: true, 
+    uniqueness: true, 
+    length: { maximum: 20 }
+
+  validates :image, 
+    presence: true
 end
