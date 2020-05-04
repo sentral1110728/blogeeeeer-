19b3c19ddfa8627e64e7c6179999daebedeff629
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-04 13:43:37 +0900

    commentの単体テスト

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it "commentが空では登録できないこと" do
+    comment = build(:comment, comment: "")
+    comment.valid?
+    expect(comment.errors[:comment]).to include("を入力してください")
+  end
+  it "commentが存在すれば登録できること" do
+    comment = build(:comment)
+    comment.valid?
+    expect(comment).to be_valid
+  end
+end
