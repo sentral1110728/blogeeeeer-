ae19dfaf8c1d99a9d4a004f6447c61c042bf9498
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-04 13:16:34 +0900

    profileの単体テスト

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :profile do
-    
+    association :user
+    appeal {"管理者です。よろしく！"}
+    user_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/猫.jpeg'), 'image/jpeg') }
   end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it "profileを登録できること" do
+    profile = build(:profile)
+    profile.valid?
+    expect(profile).to be_valid
+  end
 end
