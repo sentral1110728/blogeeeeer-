3b40b8faf0274004cdf8bb66e084c6ae182280c2
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-05 15:45:53 +0900

    開発環境と本番環境で処理を分離

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'blogeeeeer-image'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/blogeeeeer-image'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
-    aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'blogeeeeer-image'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/blogeeeeer-image'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+  else
+    config.storage = :file
+  end
 end
