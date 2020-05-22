8e9f144a731480738d5de62ffb5215a22442c85d
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-22 22:19:41 +0900

    ユーザーの権限を変更可能にする

--- app/assets/javascripts/users/index.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function(){
-  $('.JS_Click_ShowModal_Trigger').on('click', function(){
-    $('.ModalLayer').addClass('isShow');
-  });
-  // オーバーレイをタップでモーダル閉じる
-  $('.ModalLayer-Mask').on('click', function(){
-    $('.ModalLayer').removeClass('isShow');
-  });
-});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page], per_page: 10)
+    @authorities = Authority.all
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to users_path, notice: "変更完了"
+    else
+      redirect_to users_path, notice: "変更できませんでした。"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email).merge(authority_id: params[:user][:authority_id])
   end
 end
